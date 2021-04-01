6d783960b5273d40e56a35573f954155f099dcdf
Arnold Vink <info@arnoldvink.com>
2021-04-01 10:15:28 +0200

    - Added Kodi 20 support.

--- plugin.video.enigmastreamer/hybrid.py
+++ plugin.video.enigmastreamer/hybrid.py
@@ -1,21 +1,64 @@
 import sys
+import unicodedata
 
 pythonversion = sys.version_info[0]
 if pythonversion < 3:
+    import xbmc
     import urllib2
     from HTMLParser import HTMLParser
     inputstreamname = 'inputstreamaddon'
 else:
+    import xbmcvfs
     import urllib.request, urllib.parse
     from html.parser import HTMLParser
     inputstreamname = 'inputstream'
+
+#Xbmc translate path
+def xbmc_translate_path(path):
+    if pythonversion < 3:
+        return xbmc.translatePath(path)
+    else:
+        return xbmcvfs.translatePath(path)
+
+#Remove character accents from string
+def string_remove_accents(string):
+    if pythonversion < 3:
+        string = string_to_unicode(string)
+    return ''.join(c for c in unicodedata.normalize('NFKD', string) if unicodedata.category(c) != 'Mn')
+
+#Unicode to string
+def unicode_to_string(string):
+    if pythonversion < 3:
+        if type(string) == unicode:
+            return ''.join(c for c in unicodedata.normalize('NFKD', string) if unicodedata.category(c) != 'Mn')
+    return string
+
+#String to unicode
+def string_to_unicode(string):
+    if pythonversion < 3:
+        if type(string) != unicode:
+            return unicode(string, 'UTF-8')
+    return string
+
+#String encode utf8
+def string_encode_utf8(string):
+    if pythonversion < 3:
+        return string.encode('UTF-8')
+    else:
+        if type(string) == bytes:
+            return string.encode('UTF-8')
+        else:
+            return string
 
 #String decode utf8
 def string_decode_utf8(string):
     if pythonversion < 3:
         return string.decode('UTF-8')
     else:
-        return string
+        if type(string) == bytes:
+            return string.decode('UTF-8')
+        else:
+            return string
 
 #Urllib request
 def urllib_request(*args, **kwargs):
--- plugin.video.enigmastreamer/var.py
+++ plugin.video.enigmastreamer/var.py
@@ -40,7 +40,7 @@ addonname = addon.getAddonInfo('name')
 addonicon = addon.getAddonInfo('icon')
 addonversion = addon.getAddonInfo('version')
 addonpath = hybrid.string_decode_utf8(addon.getAddonInfo('path'))
-addonstorage = os.path.join(hybrid.string_decode_utf8(xbmc.translatePath('special://profile/addon_data/')), addonid)
+addonstorage = os.path.join(hybrid.string_decode_utf8(hybrid.xbmc_translate_path('special://profile/addon_data/')), addonid)
 kodiversion = xbmc.getInfoLabel('System.BuildVersion').split(' ')[0].split('-')[0]
 pythonversion = sys.version_info[0]
 
