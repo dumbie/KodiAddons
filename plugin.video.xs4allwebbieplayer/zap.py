a387600f3e4b43931e744663248dd8a0069e8e7f
Arnold Vink <vink_arnold@hotmail.com>
2024-02-04 12:29:47 +0100

    - Zap informatie is nu een verticale lijst voor betere duidelijkheid.

--- plugin.video.xs4allwebbieplayer/zap.py
+++ plugin.video.xs4allwebbieplayer/zap.py
@@ -52,11 +52,12 @@ def set_remote_number(_self, ZapNumberPress, selectMode, clickOnSelection):
     listItemCount = listContainer.size()
     for itemNum in range(0, listItemCount):
         try:
-            channelNameProp = listContainer.getListItem(itemNum).getProperty('ChannelName')
-            channelNumberProp = listContainer.getListItem(itemNum).getProperty('ChannelNumber')
-            if channelNumberProp.startswith(var.ZapNumberString):
-                channelNumberCut = channelNumberProp.replace(var.ZapNumberString, '', 1)
-                var.ZapHintString += func.get_provider_color_string() + var.ZapNumberString + '[/COLOR][COLOR gray]' + channelNumberCut + '[/COLOR] ' + channelNameProp + ' '
+            listItem = listContainer.getListItem(itemNum)
+            channelName = listItem.getProperty('ChannelName')
+            channelNumber = listItem.getProperty('ChannelNumber')
+            if channelNumber.startswith(var.ZapNumberString):
+                channelNumberCut = channelNumber.replace(var.ZapNumberString, '', 1)
+                var.ZapHintString += '[COLOR white]' + var.ZapNumberString + '[/COLOR]' + func.get_provider_color_string() + channelNumberCut + '[/COLOR]  ' + channelName + '\n'
         except:
             continue
 
@@ -83,41 +84,44 @@ def select_remote_number(_self, clickOnSelection):
 
 def zap_remote_number(_self):
     listContainer = _self.getControl(var.ZapControlId)
-    itemnum = lifunc.search_channelnumber_listcontainer(listContainer, var.ZapNumberString)
-    if itemnum == None:
+    itemNum = lifunc.search_channelnumber_listcontainer(listContainer, var.ZapNumberString)
+    if itemNum == None:
         return
 
-    listItemSelected = listContainer.getListItem(itemnum)
+    listItemSelected = listContainer.getListItem(itemNum)
     streamplay.play_tv(listItemSelected, ShowInformation=True)
 
 def thread_zap_wait_timer(_self, selectMode, clickOnSelection):
     while var.thread_zap_wait_timer.Allowed():
-        xbmc.sleep(100)
-        interactSecond = 3
-        lastInteractSeconds = int((datetime.now() - var.ZapDelayDateTime).total_seconds())
-        if var.ZapTimerForce or lastInteractSeconds >= interactSecond or len(var.ZapNumberString) == 4:
-            #Handle remote action
-            if selectMode:
-                select_remote_number(_self, clickOnSelection)
+        try:
+            xbmc.sleep(100)
+            interactSecond = 3
+            lastInteractSeconds = int((datetime.now() - var.ZapDelayDateTime).total_seconds())
+            if var.ZapTimerForce or lastInteractSeconds >= interactSecond or len(var.ZapNumberString) == 4:
+                #Handle remote action
+                if selectMode:
+                    select_remote_number(_self, clickOnSelection)
+                else:
+                    zap_remote_number(_self)
+
+                #Hide remote number popup
+                _self.setProperty('ZapVisible', 'false')
+                func.updateLabelText(_self, 7001, '')
+
+                #Reset remote variables
+                var.ZapControlId = 0
+                var.ZapNumberString = ''
+                var.ZapHintString = ''
+                var.ZapTimerForce = False
+                var.thread_zap_wait_timer.Stop()
             else:
-                zap_remote_number(_self)
+                #Countdown string
+                zapCountInt = interactSecond - lastInteractSeconds
+                zapCountString = '[COLOR gray]' + str(zapCountInt) + '[/COLOR]'
 
-            #Hide remote number popup
-            _self.setProperty('ZapVisible', 'false')
-            func.updateLabelText(_self, 7001, '')
-
-            #Reset remote variables
-            var.ZapControlId = 0
-            var.ZapNumberString = ''
-            var.ZapHintString = ''
-            var.ZapTimerForce = False
-            var.thread_zap_wait_timer.Stop()
-        else:
-            #Countdown string
-            zapCountInt = interactSecond - lastInteractSeconds
-            zapCountString = '[COLOR gray]' + str(zapCountInt) + '[/COLOR]'
-
-            #Show remote number popup
-            func.updateLabelText(_self, 7001, zapCountString)
-            func.updateLabelText(_self, 7002, var.ZapHintString)
-            _self.setProperty('ZapVisible', 'true')
+                #Show remote number popup
+                func.updateLabelText(_self, 7001, zapCountString)
+                func.updateLabelText(_self, 7002, var.ZapHintString)
+                _self.setProperty('ZapVisible', 'true')
+        except:
+            pass
